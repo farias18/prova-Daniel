716498e61d9a78f9b8f55d2ae150e8d08fa29e89
Samuel Farias <87392437+farias18@users.noreply.github.com>
2022-12-01 20:48:39 -0300

    Add files via upload
    daniel me passa

---
+++ products.js
@@ -0,0 +1,49 @@
+let scanf = require('scanf');
+
+const produtos = [
+    {
+        nome: "Notebook",
+        categoria: "Eletrônico",
+        valor: 1999.99
+    },
+    {
+        nome: "Impressora",
+        categoria: "Eletrônico",
+        valor: 999.99
+    },
+    {
+        nome: "Caneta BIC",
+        categoria: "Escolar",
+        valor: 0.50
+    },
+    {
+        nome: "Lapiseira Pentel",
+        categoria: "Escolar",
+        valor: 7.50
+    },
+]
+
+
+function listar() {
+    produtos.forEach(p => {
+        console.log(p)
+    })
+}
+
+function listarporcategoria(){
+    produtos.forEach(categoria => {
+        console.log (categoria)
+    })
+}
+
+function listarporvalor(){
+    produtos.forEach(valorminimo => {
+        console.log (valorminimo)
+    })
+    produtos.forEach(valormaximo => {
+        console.log (valormaximo)
+    })
+}
+
+
+
